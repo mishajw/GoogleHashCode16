230d5a59da33743cc04791d210717bb74602d52f
Misha wagner <mishajw@gmail.com>
2016-02-11 18:53:53 +0000

    Made drone class

--- src/Drone.java
+++ src/Drone.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by misha on 11/02/16.
  */
 public class Drone {
+	double x, y;
+	int maxPayload;
+
+	List<Product> carrying;
+
+	public Drone(double x, double y, int maxPayload) {
+		this.x = x;
+		this.y = y;
+		this.maxPayload = maxPayload;
+
+		carrying = new ArrayList<>();
+	}
+
+
 }
